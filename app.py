4122a6270c02871bebc2661df23b74ca2b9011c4
Ikal747 <andres4482@es.uazuay.edu.ec>
2025-05-21 10:19:28 -0500

    nm

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.tittle("Mi segunda app con Streamlit")
+st.title("Mi segunda app con Streamlit")
 st.header("Hola mundo")
 st.write("Este es mi primer app con Streamlit")
 st.write("Hola mund")
