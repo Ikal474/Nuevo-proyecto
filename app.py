f76fc78b4620cce6ee62a5a1133e1461017cc215
Ikal747 <andres4482@es.uazuay.edu.ec>
2025-05-21 10:42:09 -0500

    wf

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("Mi segunda app con Streamlit")
 st.header("Hola mundo")
-st.write("Este es mi primer Streamlit")
+st.write("Este es mi primr Streamlit")
 st.write("Hola mund")
 
-fig = px.bar(x=[A, B, C], y=[1, 2, 3])
+fig = px.bar(x=["A", "B", "C"], y=[1, 2, 3])
 st.plotly_chart(fig)
