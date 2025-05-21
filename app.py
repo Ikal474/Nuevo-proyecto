4b6fa060e787429c83b7414ca9be375d60cd3c7f
Ikal747 <andres4482@es.uazuay.edu.ec>
2025-05-21 10:29:40 -0500

    knk

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ st.title("Mi segunda app con Streamlit")
 st.header("Hola mundo")
 st.write("Este es mi segundo Streamlit")
 st.write("Hola mund")
+
+fig = px.bar(x=[A, B, C], y=[1, 2, 3])
+st.plotly_chart(fig)
