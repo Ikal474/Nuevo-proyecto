bf7600c2078ed0cf53f8e74ac8cf06246d242114
Ikal747 <andres4482@es.uazuay.edu.ec>
2025-05-21 10:12:19 -0500

    mb

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.tittle("Mi primera app con Streamlit")
 st.header("Hola mundo")
 st.write("Este es mi primer app con Streamlit")
-st.write("Hola mundo")
+st.write("Hola mund")
